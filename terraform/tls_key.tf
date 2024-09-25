7b7aec836bda49b00782c32637699037d91ad6e6
Julio Vinachi <julio899@gmail.com>
2024-09-25 16:27:02 -0400

    control save key

--- terraform/tls_key.tf
+++ terraform/tls_key.tf
@@ -10,12 +10,6 @@ resource "aws_key_pair" "deployer" {
   public_key = tls_private_key.key.public_key_openssh
 }
 
-# Create the Key Pair
-resource "aws_key_pair" "key_pair" {
-  key_name   = "${aws_key_pair.deployer.key_name}-${lower("stg")}"
-  public_key = tls_private_key.key_pair.public_key_openssh
-}
-
 # Save file
 resource "local_file" "ssh_key" {
   filename = "${aws_key_pair.key_pair.key_name}.pem"
