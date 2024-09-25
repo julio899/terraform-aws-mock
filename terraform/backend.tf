bcb5149d188571022471fcb01fbe2b3a3ac30e3d
Julio Vinachi <julio899@gmail.com>
2024-09-25 14:58:57 -0400

    control add keys in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     bucket  = "ng-terraform-state"
     key     = "terraform.tfstate"
-    region  = var.AWS_REGION
+    region  = "${var.AWS_REGION}"
     encrypt = true
-    access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_SECRET_ACCESS_KEY
+    access_key = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
   }
 }
